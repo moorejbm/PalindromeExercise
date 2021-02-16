df61908393fb3cf40e42ac6fbd1df5a4b376fd71
Jason Moore <moorejbm@gmail.com>
2021-02-16 14:21:41 -0600

    Update2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    //create a method (IsAPalindrome) that will take a parameter of type string and return a 
+    //value of type bool.  If the given input is a palindrome – return true, else return false. 
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversedWord = word.Reverse();
+
+            return word.ToLower().SequenceEqual(reversedWord);  
+
+        }
+
+
+
+
+
+
+            //reverse the word 
+            //var reversed = "";
+
+           // for (int i = word.Length - 1; i >= 0; i--)
+            //{
+           ///     reversed += word.ToLower()[i];  //reversed = reversed + word 
+           // }
+
+            //compare the original with thr reverse
+           // if (reversed == word.ToLower())
+           // {
+            //    return true;
+            //}
+           // else
+           // {
+           //     return false;
+           // }
+
+            //return wether they are the same or not
+        }
+    }
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,27 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        //	Write you test method in the UnitTest1.cs file 
+
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string word, bool expected)
         {
+            //arranged
+            var tester = new WordSmith();
+            //act
+            bool actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
+
 
         }
     }
